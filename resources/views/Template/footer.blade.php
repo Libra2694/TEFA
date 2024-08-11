aad84a039a04d9ec880521384d7705b9cc4eca20
Libra Studio <alifaja0411@gmail.com>
2024-08-11 20:18:40 +0700

    rws

--- resources/views/Template/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      LPPM
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2024 <a href="https://adminlte.io">E-Arsip</a>.</strong> Teknologi Rekayasa Perangkat Lunak.
-  </footer>
